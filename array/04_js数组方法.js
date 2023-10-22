c809a582046bfe54c878f21403d6514a1859da05
灿烂 <1481086138@qq.com>
2023-10-22 23:52:07 +0800

    js数组方法

--- array/04_js数组方法.js
+++ array/04_js数组方法.js
@@ -54,3 +54,60 @@ const evenNumbers = numbers.filter(isEven)
 // 如果要对数组中的元素求和，可以使用reduce 
 const sum = numbers.reduce((previousValue, current) => previousValue + current)
 // console.log(sum) // 120
+
+// es6和数组的新功能
+// 1. for of 迭代数组
+// for(const n of numbers) {
+//   console.log(n % 2 === 0 ? 'even' : 'odd')
+// }
+
+// 2. @@iterator对象
+// 通过Symbol.iterator访问迭代器对象
+// 得到迭代器
+let iterator = numbers[Symbol.iterator]()
+
+// console.log(iterator.next().value) // 1
+// console.log(iterator.next().value) // 2
+// console.log(iterator.next().value) // 3
+// console.log(iterator.next().value) // 4
+// console.log(iterator.next().value) // 5
+
+// 循环输出numbers的迭代器值
+
+// for(const n of iterator) {
+//   console.log(n)  // 1 - 15
+// }
+
+// 3.entries ： entries 返回包含键值对的@@iterator
+let aEntries = numbers.entries() // 得到键值对的迭代器
+// console.log(aEntries.next().value) // [0, 1] - 位置0的值为1
+// console.log(aEntries.next().value) // [1, 2] - 位置1的值为2
+
+// 循环输出numbers的键值对
+// for (const n of aEntries) {
+//     console.log(n)
+// }
+
+// 4.keys 返回包含数组索引的@@iterator
+const aKeys = numbers.keys()
+// console.log(aKeys.next().value) // 0
+// console.log(aKeys.next().value) // 1
+// console.log(aKeys.next().value) // 2
+// console.log(aKeys.next()) // { value: 3, done: false }
+
+// 循环输出numbers的index
+// for (const n of aKeys) {
+//   console.log(n) // 0 - 14
+// }
+
+// 5. values 返回包含数组值的@@iterator
+const aValues = numbers.values()
+// console.log(aValues.next().value) // 1
+// console.log(aValues.next().value) // 2
+// console.log(aValues.next().value) // 3
+
+// 循环输出numbers的值
+// for (const n of aValues) {
+//   console.log(n) // 1 - 15
+// }
+
